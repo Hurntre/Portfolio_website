aea4c61e59523ceefbdc4b46352a5cf508f4e4b0
Adefolaju Ariyo <adefolajuariyo@gmail.com>
2023-02-10 14:18:37 +0100

    Feat: add CV download to home page

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ ENV PATH /app/node_modules/.bin:$PATH
 # install app dependencies
 COPY package.json ./
 COPY yarn.lock ./
-RUN yarn install --silent
+RUN yarn install --legacy-peer-deps --silent
 
 # add app
 COPY . ./
--- src/header/index.js
+++ src/header/index.js
@@ -61,9 +61,15 @@ const Headermain = () => {
           </div>
           <div className='menu_footer d-flex flex-column flex-md-row justify-content-between align-items-md-center position-absolute w-100 p-3'>
             <div className='d-flex'>
-              <a href={socialprofils.linkedin}>Linkedln</a>
-              <a href={socialprofils.github}>Github</a>
-              <a href={socialprofils.twitter}>Twitter</a>
+              <a href={socialprofils.linkedin} target='_blank' rel='noreferrer'>
+                Linkedln
+              </a>
+              <a href={socialprofils.github} target='_blank' rel='noreferrer'>
+                Github
+              </a>
+              <a href={socialprofils.twitter} target='_blank' rel='noreferrer'>
+                Twitter
+              </a>
             </div>
             <p className='copyright m-0'>copyright __ {logotext}</p>
           </div>
